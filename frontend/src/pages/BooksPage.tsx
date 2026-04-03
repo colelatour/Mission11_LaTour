7ed6629f4732ec1752d2d3ae6142aa669c4b22b2
colelatour <clatour0@byu.edu>
2026-04-02 19:46:15 -0600

    Seventh Commit

--- frontend/src/pages/BooksPage.tsx
+++ frontend/src/pages/BooksPage.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import BookList from '../components/BookList';
 import CategoryFilter from '../components/CategoryFilter';
+import CartSummary from '../components/CartSummary';
 
 function BooksPage() {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
   return (
     <div className="container mt-4">
+      <CartSummary />
       <div className="row">
         <div className="col-md-3">
           <CategoryFilter
